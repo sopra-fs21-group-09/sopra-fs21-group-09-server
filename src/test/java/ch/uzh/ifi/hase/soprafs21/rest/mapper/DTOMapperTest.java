a0d460edcb3ba65ca829d029609c96541e4760f0
Loris De Luca <loris.deluca@uzh.ch>
2021-05-01 17:52:07 +0200

    first dto mapper test

--- src/test/java/ch/uzh/ifi/hase/soprafs21/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/rest/mapper/DTOMapperTest.java
@@ -1,5 +1,9 @@
 package ch.uzh.ifi.hase.soprafs21.rest.mapper;
 
+import ch.uzh.ifi.hase.soprafs21.entity.User;
+import ch.uzh.ifi.hase.soprafs21.rest.dto.User.UserPostDTO;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -7,4 +11,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Tests if the mapping between the internal and the external/API representation works.
  */
 public class DTOMapperTest {
+    @Test
+    public void testCreateUser_fromUserPostDTO_toUser_success() {
+        // create UserPostDTO
+        UserPostDTO userPostDTO = new UserPostDTO();
+        userPostDTO.setPassword("name");
+        userPostDTO.setUsername("username");
+
+        // MAP -> Create user
+        User user = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
+
+        // check content
+        assertEquals(userPostDTO.getPassword(), user.getPassword());
+        assertEquals(userPostDTO.getUsername(), user.getUsername());
+    }
 }
