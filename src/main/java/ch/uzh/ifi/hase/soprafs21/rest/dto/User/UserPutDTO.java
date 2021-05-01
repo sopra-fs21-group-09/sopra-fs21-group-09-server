2b18ab5ae83b7f264b7a594362e76d19d3e3d6f6
Loris De Luca <loris.deluca@uzh.ch>
2021-05-01 18:16:16 +0200

    user dto mapper tests

--- src/main/java/ch/uzh/ifi/hase/soprafs21/rest/dto/User/UserPutDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/rest/dto/User/UserPutDTO.java
@@ -5,12 +5,24 @@ public class UserPutDTO {
     private String name;
     private String matrikelNr;
 
+    public String getUsername() {
+        return username;
+    }
+
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getMatrikelNr() {
+        return matrikelNr;
     }
 
     public void setMatrikelNr(String matrikelNr) {
--- src/test/java/ch/uzh/ifi/hase/soprafs21/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/rest/mapper/DTOMapperTest.java
@@ -1,10 +1,13 @@
 package ch.uzh.ifi.hase.soprafs21.rest.mapper;
 
 import ch.uzh.ifi.hase.soprafs21.entity.User;
+import ch.uzh.ifi.hase.soprafs21.rest.dto.User.UserGetDTO;
 import ch.uzh.ifi.hase.soprafs21.rest.dto.User.UserPostDTO;
+import ch.uzh.ifi.hase.soprafs21.rest.dto.User.UserPutDTO;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * DTOMapperTest
@@ -15,8 +18,9 @@ public class DTOMapperTest {
     public void testCreateUser_fromUserPostDTO_toUser_success() {
         // create UserPostDTO
         UserPostDTO userPostDTO = new UserPostDTO();
-        userPostDTO.setPassword("name");
+        userPostDTO.setPassword("password");
         userPostDTO.setUsername("username");
+        userPostDTO.setName("name");
 
         // MAP -> Create user
         User user = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
@@ -24,5 +28,45 @@ public class DTOMapperTest {
         // check content
         assertEquals(userPostDTO.getPassword(), user.getPassword());
         assertEquals(userPostDTO.getUsername(), user.getUsername());
+        assertEquals(userPostDTO.getName(), user.getName());
+    }
+
+    @Test
+    public void testGetUser_fromUser_toUserGetDTO_success() {
+        // create User
+        User user = new User();
+        user.setName("Firstname Lastname");
+        user.setUsername("firstname@lastname");
+        user.setToken("1");
+        user.setMatrikelNr("00-000-000");
+
+        // MAP -> Create UserGetDTO
+        UserGetDTO userGetDTO = DTOMapper.INSTANCE.convertEntityToUserGetDTO(user);
+
+        // check content
+        assertEquals(user.getId(), userGetDTO.getId());
+        assertEquals(user.getUsername(), userGetDTO.getUsername());
+        assertEquals(user.getToken(), userGetDTO.getToken());
+        assertEquals(user.getName(), userGetDTO.getName());
+        assertEquals(user.getMatrikelNr(), userGetDTO.getMatrikelNr());
+    }
+
+    @Test void testUpdateUser_fromUserPutDTO_toUser_success() {
+        // create UserPutDTO
+        UserPutDTO userPutDTO = new UserPutDTO();
+        userPutDTO.setName("name");
+        userPutDTO.setUsername("username");
+        userPutDTO.setMatrikelNr("00-000-000");
+
+        // MAP -> Create User
+        User user = DTOMapper.INSTANCE.convertUserPutDTOtoEntity(userPutDTO);
+
+        // check content
+        assertEquals(userPutDTO.getName(), user.getName());
+        assertEquals(userPutDTO.getUsername(), user.getUsername());
+        assertEquals(userPutDTO.getMatrikelNr(), user.getMatrikelNr());
+        assertNull(user.getPassword());
+        assertNull(user.getId());
+        assertNull(user.getToken());
     }
 }
