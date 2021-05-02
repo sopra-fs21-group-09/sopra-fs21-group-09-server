9efcbbb8cb09f392b5c3c046154b8dee8ac8c515
Loris De Luca <loris.deluca@uzh.ch>
2021-05-02 19:57:17 +0200

    added some task service tests (unit testing)

--- src/test/java/ch/uzh/ifi/hase/soprafs21/service/TaskServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/service/TaskServiceTest.java
@@ -1,4 +1,105 @@
 package ch.uzh.ifi.hase.soprafs21.service;
 
+import ch.uzh.ifi.hase.soprafs21.entity.Deadline;
+import ch.uzh.ifi.hase.soprafs21.entity.Task;
+import ch.uzh.ifi.hase.soprafs21.repository.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TaskServiceTest {
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    @InjectMocks
+    private TaskService taskService;
+
+    private Task testTask;
+    private Deadline testDeadline;
+    private Task testSubTaskA;
+    private Task testSubTaskB;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        testTask = new Task();
+        testDeadline = new Deadline();
+        testSubTaskA = new Task();
+        testSubTaskB = new Task();
+
+        testTask.setDeadline(testDeadline);
+        testTask.setSubTasks(new ArrayList<Task>());
+        testTask.addSubTask(testSubTaskA);
+        testTask.addSubTask(testSubTaskB);
+
+        testTask.setId(1L);
+        testTask.setName("name");
+        testTask.setDescription("description");
+
+        testDeadline.setId(1L);
+        testDeadline.setVisible(true);
+        testDeadline.setTime(LocalDateTime.parse("2021-01-01T00:00:01"));
+
+        testSubTaskA.setId(2L);
+        testSubTaskA.setName("subA_name");
+
+        testSubTaskB.setId(3L);
+        testSubTaskB.setDescription("subB_desc");
+
+        Mockito.when(taskRepository.save(Mockito.any())).thenReturn(testTask);
+    }
+
+    @Test
+    public void createTask_validInputs_success() {
+        // when
+        Task createdTask = taskService.createTask(testTask);
+
+        // then
+        Mockito.verify(taskRepository, Mockito.times(1)).save(Mockito.any());
+
+        assertEquals(testTask.getId(), createdTask.getId());
+        assertEquals(testTask.getName(), createdTask.getName());
+        assertEquals(testTask.getDescription(), createdTask.getDescription());
+        assertEquals(testDeadline.getId(), createdTask.getDeadline().getId());
+        assertEquals(testTask.getSubTasks(), createdTask.getSubTasks());
+    }
+
+    @Test
+    public void getTask_validInput_success() {
+        // when
+        Mockito.when(taskRepository.findById(Mockito.any())).thenReturn(java.util.Optional.ofNullable(testTask));
+        Task foundTask = taskService.getTask(testTask.getId());
+
+        // then
+        Mockito.verify(taskRepository, Mockito.times(2)).findById(Mockito.any());
+
+        assertEquals(testTask.getId(), foundTask.getId());
+        assertEquals(testTask.getName(), foundTask.getName());
+        assertEquals(testTask.getDescription(), foundTask.getDescription());
+        assertEquals(testDeadline.getId(), foundTask.getDeadline().getId());
+        assertEquals(testTask.getSubTasks(), foundTask.getSubTasks());
+    }
+
+    @Test
+    public void getTask_invalidInput_throwsException() {
+        // when
+        Mockito.when(taskRepository.findById(Mockito.any())).thenReturn(Optional.empty());
+
+        //then
+        assertThrows(ResponseStatusException.class, () -> taskService.getTask(testTask.getId()));
+    }
+
 }
