3ecb80e08e4d7831ee0c00277a259b08d383fbf4
Loris De Luca <loris.deluca@uzh.ch>
2021-05-02 15:17:14 +0200

    task repository integration test

--- src/test/java/ch/uzh/ifi/hase/soprafs21/repository/TaskRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/repository/TaskRepositoryIntegrationTest.java
@@ -1,13 +1,25 @@
 package ch.uzh.ifi.hase.soprafs21.repository;
 
 
+import ch.uzh.ifi.hase.soprafs21.entity.Deadline;
+import ch.uzh.ifi.hase.soprafs21.entity.Task;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @DataJpaTest
 public class TaskRepositoryIntegrationTest {
+
+    private Task task;
 
     @Autowired
     private TestEntityManager entityManager;
@@ -16,4 +28,49 @@ public class TaskRepositoryIntegrationTest {
     @Autowired
     private TaskRepository taskRepository;
 
+    @Qualifier("deadlineRepository")
+    @Autowired
+    private DeadlineRepository deadlineRepository;
+
+    @BeforeEach
+    public void setUp() {
+        Deadline deadline = new Deadline();
+        deadline.setVisible(false);
+        deadline.setTime(LocalDateTime.parse("2021-01-01T12:00"));
+
+        task = new Task();
+        task.setName("name");
+        task.setDescription("description");
+        task.setDeadline(deadline);
+
+        Task subTask = new Task();
+        subTask.setName("sub_name");
+        subTask.setDescription("sub_desc");
+        subTask.setDeadline(new Deadline());
+        subTask.getDeadline().setTime(LocalDateTime.parse("2021-01-01T10:00"));
+        subTask.getDeadline().setVisible(false);
+        task.setSubTasks(new ArrayList<>());
+        task.addSubTask(subTask);
+
+        entityManager.persist(task);
+        entityManager.persist(subTask);
+    }
+
+    @Test
+    public void findById_success() {
+        // when
+        Task found = taskRepository.findById(task.getId()).get();
+
+        // then
+        assertNotNull(found.getId());
+        assertNotNull(found.getDeadline().getId());
+        assertEquals(task.getName(), found.getName());
+        assertEquals(task.getDescription(), found.getDescription());
+        assertEquals(task.getId(), found.getDeadline().getId());
+        assertEquals(task.getDeadline().getTime(), found.getDeadline().getTime());
+        assertEquals(task.getDeadline().getVisible(), found.getDeadline().getVisible());
+        assertEquals(task.getSubTasks(), found.getSubTasks());
+        assertEquals(task.getSubTasks().get(0).getId(), found.getSubTasks().get(0).getId());
+        assertEquals(task.getId(), found.getSubTasks().get(0).getParentTask().getId());
+    }
 }
