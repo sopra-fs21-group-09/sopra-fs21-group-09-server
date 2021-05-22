44ccd4db850aa70342134ad3c42e661ed71ad9d8
Loris De Luca <loris.deluca@uzh.ch>
2021-05-22 17:10:24 +0200

    close #48, close #63, close #71

--- src/main/java/ch/uzh/ifi/hase/soprafs21/websockets/DocumentController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/websockets/DocumentController.java
@@ -1,9 +1,7 @@
 package ch.uzh.ifi.hase.soprafs21.websockets;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
