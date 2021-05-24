66791622f98ccf803c49e748672eaf7cf0d884f6
Loris De Luca <loris.deluca@uzh.ch>
2021-05-24 11:22:55 +0200

    ADD MessageBean

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/websockets/MessageBean.java
@@ -0,0 +1,23 @@
+package ch.uzh.ifi.hase.soprafs21.websockets;
+
+//testing TODO: remove later
+public class MessageBean {
+    private String name;
+    private String message;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
